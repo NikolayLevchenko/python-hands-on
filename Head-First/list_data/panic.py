cc01b37735f87b7f3c365ff4d835dd41467b1678
Nikolay Levchenko <nikolay.levchenko@zalando.de>
2019-03-10 16:34:26 +0100

    added various list manipulations

--- Head-First/list_data/panic.py
+++ Head-First/list_data/panic.py
@@ -5,21 +5,31 @@ plist = list(phrase)
 print(phrase)
 print(plist)
 
-for g in range(4):
-    plist.pop()
+###################################### version 1
+# for i in range(4):
+#     plist.pop()
+# plist.pop(0)
+# plist.remove("'")
+# plist.extend([plist.pop(), plist.pop()])
+# plist.insert(2, plist.pop(3))
+# new_phrase = ''.join(plist)
 
-plist.pop(0)
-plist.remove("'")
-plist.extend([plist.pop(), plist.pop()])
-plist.insert(2, plist.pop(3))
+###################################### version 2
+# plist = plist[1:8]
+# plist.remove("'")
+# plist.insert(2, plist.pop(3))
+# plist.insert(4, plist.pop())
 
+###################################### version 3
+new_phrase = ''.join(plist[1:3])
+new_phrase = new_phrase + ''.join([plist[5], plist[4], plist[7], plist[6]])
 
-new_phrase = ''.join(plist)
+# new_phrase = ''.join(plist)
 print(plist)
 print(new_phrase)
 
-book = "The Hitchhiker's Guide to the Galaxy"
-booklist = list(book)
-print(booklist)
-backwards = booklist[::-1]
-print(''.join(backwards))
+# book = "The Hitchhiker's Guide to the Galaxy"
+# booklist = list(book)
+# print(booklist)
+# backwards = booklist[::-1]
+# print(''.join(backwards))
