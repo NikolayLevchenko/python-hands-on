8750a8796cd76f95489497f23abaaf935145e731
Nikolay Levchenko <nik.and.levchenko@gmail.com>
2019-04-11 18:51:18 +0200

    tested the code for compliance

--- Head-First/data_structures/modules/functions.py
+++ Head-First/data_structures/modules/functions.py
@@ -10,25 +10,6 @@ def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
     """the function returns a set of letters found in 'phrase'."""
     found = set(letters).intersection(set(phrase))
     return found
-
-
 # print(search_for_letters('give me an example of your high performance'))
 #
 # print(search_for_letters(letters='aeiou', phrase="Let's go play basketball"))
-
-def double(arg):
-    print('Before: ', arg)
-    arg = arg * 2
-    print('After: ', arg)
-
-
-def change(arg):
-    print('Before: ', arg)
-    arg.append('More data')
-    print('After: ', arg)
-
-
-numbers = [23, 25, 93]
-
-change(numbers)
-print(numbers)
