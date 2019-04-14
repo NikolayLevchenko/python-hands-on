faf2cd9d624c15a5467d1f3e62faec2f96858c06
Nikolay Levchenko <nik.and.levchenko@gmail.com>
2019-04-14 23:52:32 +0200

    Added log file

--- Head-First/flask_app/functions/help_functions.py
+++ Head-First/flask_app/functions/help_functions.py
@@ -6,3 +6,8 @@ def search_for_vowels(phrase: str) -> set:
 def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
     """Returns the set of 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', mode='a') as log:
+        print(req, res, file=log)
--- Head-First/flask_app/vsearch4web.py
+++ Head-First/flask_app/vsearch4web.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from functions import search4letters
+from flask import Flask, render_template, request, escape
+from functions import help_functions
 
 app = Flask(__name__)
 
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    results = str(search4letters.search_for_letters(phrase, letters))
+    results = str(help_functions.search_for_letters(phrase, letters))
+    help_functions.log_request(request, results)
     return render_template('results.html',
                            the_letters=letters,
                            the_phrase=phrase,
@@ -20,6 +21,13 @@ def do_search() -> 'html':
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
 
 
 if __name__ == '__main__':
