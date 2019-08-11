a545dbef3f4fe4a5ca40934626ec5602911a2746
Nikolay Levchenko <nik.and.levchenko@gmail.com>
2019-08-12 00:20:47 +0200

    Added context management protocol

--- Head-First/flask_app/functions/help_functions.py
+++ Head-First/flask_app/functions/help_functions.py
@@ -1,6 +1,3 @@
-import mysql.connector
-
-
 def search_for_vowels(phrase: str) -> set:
     """Returns the set of vowels found in 'phrase'."""
     return set('aeiou').intersection(set(phrase))
@@ -9,29 +6,3 @@ def search_for_vowels(phrase: str) -> set:
 def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
     """Returns the set of 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
-
-
-# def log_request(req: 'flask_request', res: str) -> None:
-#     with open('vsearch.log', mode='a') as log:
-#         print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-
-
-def log_request(req: 'flask_request', res: str) -> None:
-    dbconfig = {'host': '127.0.0.1',
-                'user': 'vsearch',
-                'password': 'vsearchpasswd',
-                'database': 'vsearchlogDB', }
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """insert into log
-    (phrase, letters, ip, browser_string, results)
-    values
-    (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res,))
-    conn.commit()
-    cursor.close()
-    conn.close()
